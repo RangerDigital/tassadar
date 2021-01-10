0a2fdb6dbf76105030129f36251a1d0343cf577a
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-01-10 10:37:34 +0100

    Add monitoring system

---
+++ monitoring/main.py
@@ -0,0 +1,46 @@
+import psutil
+import uvicorn
+from fastapi import FastAPI
+from mcstatus import MinecraftServer
+
+app = FastAPI()
+
+
+@app.get("/")
+async def status():
+    response = {}
+
+    # CPU
+    response["cpu_usage"] = psutil.cpu_percent(interval=1)
+
+    # RAM
+    memory = psutil.virtual_memory()
+    response["ram_usage"] = round(memory.percent, 1)
+    response["ram_total"] = memory.total
+    response["ram_available"] = memory.available
+    response["ram_used"] = memory.used
+
+    # LOADS
+    response["load_1"], response["load_5"], response["load_15"] = psutil.getloadavg()
+
+    # DISK - VOLUME
+    try:
+        volume = psutil.disk_usage("/mnt/volume-tassadar/")
+
+        response["volume_usage"] = volume.percent
+        response["volume_total"] = volume.total
+        response["volume_used"] = volume.used
+    except:
+        pass
+
+    # DISK - ROOT
+    try:
+        volume = psutil.disk_usage("/")
+
+        response["storage_usage"] = volume.percent
+        response["storage_total"] = volume.total
+        response["storage_used"] = volume.used
+    except:
+        pass
+
+    return response
